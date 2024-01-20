703123ec69586cb42a5bcba15d5d56c12c7aa3eb
omarfrikha96 <omar.frikha96@gmail.com>
2024-01-20 16:25:11 +0100

    add Fronts assignment

---
+++ Fronts/main.js
@@ -0,0 +1,37 @@
+class Node {
+  //create a node class
+  constructor(data) {
+    this.data = data; //give the node a data property
+    this.next = null; // pointer to the next node
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null; //first node in the list
+  }
+  addFront(value) {
+    var newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
+    return this.head; // or you can return return this;
+  }
+  removeFront() {
+    if (this.head) {
+      this.head = this.head.next;
+    }
+    return this;
+  }
+  front() {
+    if (this.head) {
+      return this.head.data;
+    }
+    return null;
+  }
+}
+
+SLL1 = new LinkedList();
+console.log(SLL1.addFront(18));
+console.log(SLL1.addFront(20));
+console.log(SLL1.removeFront());
+console.log(SLL1.front());
